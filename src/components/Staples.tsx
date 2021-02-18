9ba72a3e929dde677db721689c3c87261707db24
Kristaps Kalnins <kkkal@kth.se>
2021-02-18 16:29:36 +0200

    Staples component file

---
+++ src/components/Staples.tsx
@@ -0,0 +1,74 @@
+import React, { useContext } from "react";
+import ReactApexChart from "react-apexcharts";
+import Context from "../context";
+
+export default function Staples(){
+  const context = useContext(Context);
+
+  var state = {
+    
+    series: [{
+      name: 'Net Profit',
+      data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
+    }, {
+      name: 'Revenue',
+      data: [76, 85, 101, 98, 87, 105, 91, 114, 94]
+    }, {
+      name: 'Free Cash Flow',
+      data: [35, 41, 36, 26, 45, 48, 52, 53, 41]
+    }],
+    options: {
+      chart: {
+        type: 'bar',
+        height: 350
+      },
+      plotOptions: {
+        bar: {
+          horizontal: false,
+          columnWidth: '55%',
+          endingShape: 'rounded'
+        },
+      },
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        show: true,
+        width: 2,
+        colors: ['transparent']
+      },
+      xaxis: {
+        categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
+      },
+      yaxis: {
+        title: {
+          text: '$ (thousands)'
+        }
+      },
+      fill: {
+        opacity: 1
+      },
+      tooltip: {
+        y: {
+          formatter: function (val: any) {
+            return "$ " + val + " thousands"
+          }
+        }
+      }
+    },
+  };
+
+
+  return(
+      <div id="chart">
+        <ReactApexChart 
+          options={state.options} 
+          series={state.series}  
+          type="bar" 
+          height={350}
+        />
+      </div>
+  );
+}
+
+
